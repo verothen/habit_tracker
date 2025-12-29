1da9e4c4a8d0f766e0e4902beb77319681f92a24
verothen <vjthen@ucdavis.edu>
2025-12-29 15:46:34 -0800

    working

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ var currentDay = date.getDay();
 var currentDate = date.getDate();
 var currentYear = date.getFullYear();
 
-console.log(currentMonth); //current month - 1
-console.log(currentDay); //day of the week
-console.log(currentDate); //current date/number
-console.log(currentYear); //current year
+console.log("The current month is "+currentMonth); //current month - 1
+console.log("The current weekday is "+currentDay); //day of the week
+console.log("The current date is "+currentDate); //current date/number
+console.log("The current year is "+currentYear); //current year
 
 // IMPORTANT DATE INFO
 var months = [
@@ -38,9 +38,21 @@ habitTitle.onclick = function(){
 
 // SET THE TOTAL DAYS
 var daysInTheMonthList = [31,28,31,30,31,30,31,31,30,31,30,31];
-var daysInThisMonth = daysInTheMonthList[currentMonth];
+var daysInThisMonth = daysInTheMonthList;[currentMonth];
 
 var daysCompleted = 0;
-var totalDays = document.getElementById("totalDays");
+var totalDays = document.getElementById;("totalDays");
 
-//stopped at 4:09
+//SET UP THE CALENDAR DAYS
+var dayCount = 0;
+var rowCount = 0;
+var days = document.getElementsByClassName("days");
+
+for(var i=0;i<days.length; i++){
+    var day = days[rowCount].getElementsByClassName("day");
+    for (var j=0;j<day.length;j++){
+        if(dayCount==currentDate-1){
+            day[j].setAttribute("style","border:2px solid black");
+        }
+    }
+}
